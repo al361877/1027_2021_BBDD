3e5891047d6726fb5253de6f23f212ef1bec3eef
al361877 <35959300+al361877@users.noreply.github.com>
2021-04-10 00:09:39 +0200

    Update tablas.sql

--- tablas.sql
+++ tablas.sql
@@ -4,6 +4,7 @@ CREATE TABLE Usuario(
     nombre_y_apellidos VARCHAR(20) not null,
     telefono INTEGER not null, 
     correo VARCHAR(20) not null,
+    contraseña, 
     edad INTEGER not null,
     direccion VARCHAR(20) not null,
     tipo_usuario VARCHAR(10) not null, -- si es ciudadano, controlador o gestor municipal
