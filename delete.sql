5d53be0232789f81bdbdb22382e16d058bc5428c
al361877 <35959300+al361877@users.noreply.github.com>
2021-04-14 23:11:39 +0200

    Create delete.sql

---
+++ delete.sql
@@ -0,0 +1,26 @@
+delete from Municipio;
+delete from EspacioPublico;
+delete from Zona;
+delete from Usuario;
+delete from Estacion;
+delete from Direccion;
+delete from Controlador;
+delete from Ciudadano;
+delete from Gestor;
+delete from Reservar; 
+delete from Servicio;
+delete from FranjaEspacio;
+
+--------------------------------------------
+DROP table Servicio;
+DROP table Estacion;
+DROP table Reservar; 
+DROP table Zona;
+DROP table Controlador;
+DROP table Gestor;
+DROP table Ciudadano;
+DROP table Usuario;
+DROP table Direccion;
+DROP table FranjaEspacio;
+DROP table EspacioPublico;
+DROP table Municipio;
