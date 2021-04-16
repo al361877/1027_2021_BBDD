b43e4a0927b8f802ed37282dc7cdc2a9ae981adf
al361877 <35959300+al361877@users.noreply.github.com>
2021-04-16 20:52:48 +0200

    Update resert.sql

--- resert.sql
+++ resert.sql
@@ -1,3 +1,27 @@
+delete from Municipio;
+delete from EspacioPublico;
+delete from Zona;
+delete from Usuario;
+delete from Estacion;
+delete from Controlador;
+delete from Ciudadano;
+delete from Gestor;
+delete from Reserva; 
+delete from Servicio;
+delete from FranjaEspacio;
+
+--------------------------------------------
+DROP table Servicio;
+DROP table Estacion;
+DROP table Reserva; 
+DROP table Zona;
+DROP table Controlador;
+DROP table Gestor;
+DROP table Ciudadano;
+DROP table Usuario;
+DROP table FranjaEspacio;
+DROP table EspacioPublico;
+DROP table Municipio;
 
 
 
