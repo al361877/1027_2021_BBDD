f895743615f7e955c79483a9a61b69d2d2c9b35c
al361877 <35959300+al361877@users.noreply.github.com>
2021-04-16 12:59:44 +0200

    Update resert.sql

--- resert.sql
+++ resert.sql
@@ -3,7 +3,7 @@ delete from EspacioPublico;
 delete from Zona;
 delete from Usuario;
 delete from Estacion;
-delete from Direccion;
+
 delete from Controlador;
 delete from Ciudadano;
 delete from Gestor;
@@ -20,7 +20,7 @@ DROP table Controlador;
 DROP table Gestor;
 DROP table Ciudadano;
 DROP table Usuario;
-DROP table Direccion;
+
 DROP table FranjaEspacio;
 DROP table EspacioPublico;
 DROP table Municipio;
