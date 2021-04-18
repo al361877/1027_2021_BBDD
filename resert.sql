ffa32851785d69df56c8a6d8ef99781c1c66dbbb
Dan <36074341+DanPeia96@users.noreply.github.com>
2021-04-18 19:19:51 +0200

    Update resert.sql

--- resert.sql
+++ resert.sql
@@ -146,6 +146,24 @@ CREATE TABLE Servicio(
 
 
 
+INSERT INTO Municipio VALUES('cs', 'Castellón', '39°58′N 0°03′O', 150000);
+INSERT INTO Municipio VALUES('vlc', 'Valencia', '39°28′12″N 0°22′35″O', 2850000);
+INSERT INTO Municipio VALUES('alc', 'Alicante', '38°21′N 0°29′W', 240000);
+INSERT INTO Municipio VALUES('naq', 'Náquera', '39°48′N 0°33′O', 40000);
+INSERT INTO Municipio VALUES('xat', 'Xativa', '39°58′N 0°53′O', 50000);
+
+INSERT INTO EspacioPublico VALUES('rio1', 'alc', 'rio', 3004);
+INSERT INTO EspacioPublico VALUES('rio2', 'alc', 'rio', 3004);
+INSERT INTO EspacioPublico VALUES('des4', 'cs', 'otros', 12004);
+INSERT INTO EspacioPublico VALUES('herm3', 'xat', 'otros', 10004);
+INSERT INTO EspacioPublico VALUES('mgd2', 'cs', 'otros', 10004);
+INSERT INTO EspacioPublico VALUES('mec2', 'naq', 'otros', 11004);
+
+INSERT INTO FranjaEspacio VALUES ('d1', 21/05/2019, 21/05/2019, 9:00, 12:00)
+INSERT INTO FranjaEspacio VALUES ('d2', 21/07/2019, 22/07/2019, 12:00, 12:00)
+INSERT INTO FranjaEspacio VALUES ('d3', 27/04/2019, 21/05/2019, 9:00, 12:00)
+INSERT INTO FranjaEspacio VALUES ('d4', 21/05/2020, 21/05/2020, 11:00, 18:00)
+INSERT INTO FranjaEspacio VALUES ('d5', 14/09/2021, 21/10/2021, 9:00, 12:00)
 
 
 INSERT INTO Usuario VALUES ('A1234509','gemmen', 'GEMMA MENGUAL', '645456564', 'gemmen@hotmail.com', 'pass', '12-3-1994', 'iddir123456789','asdfgas',12345, 'Ciudadano');
@@ -174,25 +192,11 @@ INSERT INTO Ciudadano VALUES ('A1345092');
 INSERT INTO Ciudadano VALUES ('G1245093');
 INSERT INTO Ciudadano VALUES ('R1235094');
 
-INSERT INTO Municipio VALUES('cs', 'Castellón', '39°58′N 0°03′O', 150000);
-INSERT INTO Municipio VALUES('vlc', 'Valencia', '39°28′12″N 0°22′35″O', 2850000);
-INSERT INTO Municipio VALUES('alc', 'Alicante', '38°21′N 0°29′W', 240000);
-INSERT INTO Municipio VALUES('naq', 'Náquera', '39°48′N 0°33′O', 40000);
-INSERT INTO Municipio VALUES('xat', 'Xativa', '39°58′N 0°53′O', 50000);
-
 INSERT INTO Gestor VALUES ('G1245493', 'cs');
 INSERT INTO Gestor VALUES ('A1234596', 'vlc');
 INSERT INTO Gestor VALUES ('I1234508', 'alc');
 INSERT INTO Gestor VALUES ('U2234509', 'xat');
 INSERT INTO Gestor VALUES ('A3234509', 'naq');
-
-INSERT INTO EspacioPublico VALUES('rio1', 'alc', 'rio', 3004);
-INSERT INTO EspacioPublico VALUES('rio2', 'alc', 'rio', 3004);
-INSERT INTO EspacioPublico VALUES('des4', 'cs', 'otros', 12004);
-INSERT INTO EspacioPublico VALUES('herm3', 'xat', 'otros', 10004);
-INSERT INTO EspacioPublico VALUES('mgd2', 'cs', 'otros', 10004);
-INSERT INTO EspacioPublico VALUES('mec2', 'naq', 'otros', 11004);
-
 
 INSERT INTO Controlador VALUES ('H4234509', 'mgd2');
 INSERT INTO Controlador VALUES ('G5234509', 'herm3');
@@ -200,4 +204,28 @@ INSERT INTO Controlador VALUES ('F6234509', 'rio1');
 INSERT INTO Controlador VALUES ('K7234509', 'rio2');
 INSERT INTO Controlador VALUES ('T8234509', 'des4');
 INSERT INTO Controlador VALUES ('E9234509', 'mec2');
+
+INSERT INTO  Zona VALUES ('1A', 'norte', 'mgd2', 10004, 'firme', 'tierra');
+INSERT INTO  Zona VALUES ('1B', 'sur', 'mgd2', 11004, 'firme', 'tierra');
+INSERT INTO  Zona VALUES ('1C', 'este', 'mec2', 10004, 'firme', 'tierra');
+INSERT INTO  Zona VALUES ('2A', 'norte', 'mgd2', 10004, 'firme', 'tierra');
+INSERT INTO  Zona VALUES ('3A', 'oeste', 'rio2', 3004, 'firme', 'tierra');
+
+INSERT INTO Reserva VALUES ('cs1', 'A1345092', 'd2', 'rio2', 'finUso');
+INSERT INTO Reserva VALUES ('cs2', 'A1345092', 'd4', 'rio2', 'pendienteDeUso');
+INSERT INTO Reserva VALUES ('alc1', 'G1245093', 'd5', 'rio1', 'pendienteDeUso');
+INSERT INTO Reserva VALUES ('xat1', 'R1235094', 'd1', 'herm3', 'finUso');
+INSERT INTO Reserva VALUES ('vlc1', 'A1234509', 'd3', 'rio2', 'CanceladoUuario');
+
+INSERT INTO Estacion VALUES ('aa', 12/08/2019. 12/08/2019);
+INSERT INTO Estacion VALUES ('bb', 12/08/2019. 12/08/2019);
+INSERT INTO Estacion VALUES ('cc', 12/08/2019. 12/08/2019);
+INSERT INTO Estacion VALUES ('dd', 12/08/2019. 12/08/2019);
+INSERT INTO Estacion VALUES ('ee', 12/08/2019. 12/08/2019);
+
+INSERT INTO Servicio VALUES ('X1', 'CCCC', 'aa');
+INSERT INTO Servicio VALUES ('X2', 'BBBB', 'aa');
+INSERT INTO Servicio VALUES ('X3', 'CCCC', 'dd');
+INSERT INTO Servicio VALUES ('X4', 'DDDD', 'dd');
+INSERT INTO Servicio VALUES ('X5', 'TTTT', 'cc');
 
